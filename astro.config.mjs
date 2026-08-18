f92a093d2f339973afff3b14bd131a24db678ff1
bwlee_42 <sk92629915@gmail.com>
2026-08-18 14:28:30 +0900

    Enhance markdown with math support
    Added support for mathematical notation in markdown using remark-math and rehype-katex.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   integrations: [
     mdx(),
-    icon()
+    icon(),
   ],
+
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+
   devToolbar: {
     enabled: false,
   },
 });
-
 
